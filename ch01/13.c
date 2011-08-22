c052f43d0ec450c7203a2203e79ae6ddbe1b3045
Faheem Patel <faheem@faheempatel.com>
2011-08-22 08:26:08 +0100

    updated comments

--- ch01/13.c
+++ ch01/13.c
@@ -1,16 +1,21 @@
+// Exercise 1-13
+// Write a program to print a histogram of the lengths of
+// words in its input.  
+
 #include <stdio.h>
-#define YES 1
-#define NO 0
+
+#define YES 1 // is a word
+#define NO 0 // is not a word
 
 main()
 {
 	int is_word = NO;
 	int count = 0;
-	int ndigit[10];
+	int word_nums[10];
 	int c, i, x;
 	
 	for (i = 0; i < 10; ++i){
-		ndigit[i] = 0;
+		word_nums[i] = 0;
 	}
 
 	while ((c = getchar()) != EOF){
@@ -18,7 +23,7 @@ main()
 		if (c == ' ' || c == '\n' || c == '\t'){
 			if (is_word == YES){
 				is_word = NO;
-				++ndigit[count-1];
+				++word_nums[count-1];
 				count = 0;
 			}
 				
@@ -34,7 +39,7 @@ main()
 	for (i = 0; i < 10; ++i){
 		printf("%d: ", i+1);
 
-		for (x = 0; x < ndigit[i]; ++x){
+		for (x = 0; x < word_nums[i]; ++x){
 			printf("|");
 		}
 
